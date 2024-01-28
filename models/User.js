cd95b87bb5380690e8da1396fd6bb2dab61383c5
alextor26 <alextor2625@gmail.com>
2024-01-28 17:26:00 -0400

    Refactored user model schema using separate identification and address schemas.

--- models/User.js
+++ models/User.js
@@ -1,23 +1,56 @@
-const { model, Schema } = require('mongoose')
+const { model, Schema } = require("mongoose");
 
-const userSchema = new Schema({
+const identificationSchema = new Schema({
+  identificationType: {
+    type: String,
+    required: true,
+    enum: [
+      "US Voter Registration",
+      "PR Voter Registration",
+      "US Passport",
+      "US License",
+      "PR Licence",
+    ],
+  },
+  identificationNumber: {
+    type: String,
+    required: true,
+    // unique: true
+  },
+});
 
-    fullName: {type: String, trim: true},
-    email: {type: String, required: true, trim: true},
-    password: {type: String, required: true, trim: true},
+const addressSchema = new Schema({
+    street: { type: String, trim: true },
+    city: { type: String, trim: true },
+    state: { type: String, trim: true },
+    zip: { type: String, trim: true }
+  });
+
+const userSchema = new Schema(
+  {
+    fullName: { type: String, trim: true },
+    email: { type: String, required: true, trim: true },
+    password: { type: String, required: true, trim: true },
     userName: String,
     phoneNumber: String,
-    address: Object,
-    voterRegistrationNumber: String,
-    driversLicenseNumber: String,
-    usPassportNumber: String,
+    address: addressSchema,
+    identification: identificationSchema,
     volunteer: Boolean,
-    donations: [{type: Schema.Types.ObjectId, ref: "Donation"}],
-    eventsAttended: [{type: Schema.Types.ObjectId, ref: "Event"}]
+    donations: [{ type: Schema.Types.ObjectId, ref: "Donation" }],
+    eventsAttended: [{ type: Schema.Types.ObjectId, ref: "Event" }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-},
-{
-    timestamps: true
-})
+const User = model("User", userSchema);
+const Identification = model("Identification", identificationSchema);
+const Address = model("Address", addressSchema);
 
-module.exports = model("User", userSchema)
+
+module.exports = {
+  User,
+  Identification,
+  Address
+};
