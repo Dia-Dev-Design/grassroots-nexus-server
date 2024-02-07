3945e5c6eb7724a0e51882a0ca6bef620c331d9a
alextor26 <alextor2625@gmail.com>
2024-02-07 19:48:26 -0400

    address and identification are now objects with specifications

--- models/User.js
+++ models/User.js
@@ -1,40 +1,34 @@
 const { model, Schema } = require("mongoose");
 
-const identificationSchema = new Schema({
-  identificationType: {
-    type: String,
-    required: true,
-    enum: [
-      "US Voter Registration",
-      "PR Voter Registration",
-      "US Passport",
-      "US License",
-      "PR Licence",
-    ],
-  },
-  identificationNumber: {
-    type: String,
-    required: true,
-    // unique: true
-  },
-});
-
-const addressSchema = new Schema({
-    street: { type: String, trim: true },
-    city: { type: String, trim: true },
-    state: { type: String, trim: true },
-    zip: { type: String, trim: true }
-  });
-
 const userSchema = new Schema(
   {
     fullName: { type: String, trim: true },
     email: { type: String, required: true, trim: true },
-    password: { type: String, required: true, trim: true },
-    userName: String,
-    phoneNumber: String,
-    address: addressSchema,
-    identification: identificationSchema,
+    password: { type: String, trim: true },
+    userName: {type:String, trim:true},
+    phoneNumber: {type:String, trim:true},
+    address: {
+      street: { type: String, trim: true, default: ""},
+      city: { type: String, trim: true, default: "" },
+      state: { type: String, trim: true, default: "" },
+      zip: { type: String, trim: true, default: "" }
+    },
+    identification: {
+      identificationType: {
+        type: String,
+        enum: [
+          "US Voter Registration",
+          "PR Voter Registration",
+          "US Passport",
+          "US License",
+          "PR Licence"
+        ],
+      },
+      identificationNumber: {
+        type: String,
+        default: ""
+      },
+    },
     volunteer: Boolean,
     donations: [{ type: Schema.Types.ObjectId, ref: "Donation" }],
     eventsAttended: [{ type: Schema.Types.ObjectId, ref: "Event" }],
@@ -44,13 +38,5 @@ const userSchema = new Schema(
   }
 );
 
-const User = model("User", userSchema);
-const Identification = model("Identification", identificationSchema);
-const Address = model("Address", addressSchema);
 
-
-module.exports = {
-  User,
-  Identification,
-  Address
-};
+module.exports = model("User", userSchema)
