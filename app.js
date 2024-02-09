028a2112311da62a5da6517d951431f9fe47249e
Luis Ortiz <luis.ortiz7905@gmail.com>
2024-02-08 21:13:43 -0400

    Created Admin router

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
 mongoose
   .connect(process.env.MONGODB_URI)
