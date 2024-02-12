10024292068234995359137ec0b19b28b112220e
Test <Hammertyme101@gmail.com>
2024-02-12 16:27:53 -0500

    modelfordonor

---
+++ models/Donor.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+const donorSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    identificationInfo: { type: String, required: true },
+    donations: [{ type: Schema.Types.ObjectId, ref: 'Donation' }],
+    totalDonationAmount: { type: Number, default: 0 }
+}, { timestamps: true });
+
+module.exports = model("Donor", donorSchema);
+
