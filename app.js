f4ff7096b08f1562a8785089add130c0ba9a63c9
Test <Hammertyme101@gmail.com>
2024-02-14 21:02:49 -0500

    I made a quick sign in route and used in app.js

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/easysignup', easysignupRoutes)
 
 mongoose
   .connect(process.env.MONGODB_URI)
